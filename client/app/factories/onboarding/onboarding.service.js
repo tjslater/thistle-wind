c416e6bb7c6b3de307dfb2030fe13e8383adb612
James Slater <slaterj@google.com>
2016-02-27 18:50:03 -0800

    onboarding images onboarding html onboarding scss onboarding service gets filled out

--- client/app/factories/onboarding/onboarding.service.js
+++ client/app/factories/onboarding/onboarding.service.js
@@ -5,58 +5,107 @@ angular.module('thistleWindApp')
     // Service logic
     // ...
 
+
     var choices = [
       {
-        text: "",
+        text: "You’ve been known to look magnificent in [business casual] outfits",
         img: "",
-        selected: false
+        selected: false,
+        class: "",
+        bgs: [
+          'outfit1bg'
+        ],
+        overlay: {
+          text: "business casual",
+          id: "dialog1"
+        }
       },
       {
         text: "",
         img: "",
-        selected: false
+        selected: false,
+        class: ""
       },
       {
         text: "",
         img: "",
-        selected: false
+        selected: false,
+        class: ""
+      },
+      {
+        text: "that are also a little [rough around the edges].",
+        img: "",
+        selected: false,
+        class: "",
+        bgs: [
+          'outfit4bg',
+          'outfit4bg2'
+        ],
+        overlay: {
+          text: "rough around the edges",
+          id: "dialog2"
+        }
       },
       {
         text: "",
         img: "",
-        selected: false
+        selected: false,
+        class: ""
+      },
+      {
+        text: "But you’re scarcely less well dressed in [sneaker smart] duds",
+        img: "",
+        selected: false,
+        class: "",
+        bgs: [
+          'outfit6bg',
+          'outfit6bg2'
+        ],
+        overlay: {
+          text: "sneaker smart",
+          id: "dialog3"
+        }
       },
       {
         text: "",
         img: "",
-        selected: false
+        selected: false,
+        class: ""
       },
       {
         text: "",
         img: "",
-        selected: false
+        selected: false,
+        class: ""
       },
       {
         text: "",
         img: "",
-        selected: false
+        selected: false,
+        class: ""
       },
       {
         text: "",
         img: "",
-        selected: false
+        selected: false,
+        class: ""
       },
       {
-        text: "",
+        text: "that keep things [simple and sharp]",
         img: "",
-        selected: false
-      },
-      {
-        text: "",
-        img: "",
-        selected: false
+        selected: false,
+        class: "",
+        bgs: [
+          'outfit11bg',
+          'outfit11bg'
+        ],
+        overlay: {
+          text: "simple and sharp",
+          id: "dialog4"
+        }
+
       }
-    ]
+    ];
 
     // Public API here
     return {
--- client/app/onboarding/onboarding.controller.js
+++ client/app/onboarding/onboarding.controller.js
@@ -3,8 +3,16 @@
 angular.module('thistleWindApp')
   .controller('OnboardingCtrl', function ($scope, Onboarding) {
     $scope.choices =  Onboarding.choices;
+    $scope.selected = [];
 
     $scope.select = function (idx) {
+      var selection = $scope.choices[idx];
+      if ($scope.selected.indexOf(selection) === -1){
+        $scope.selected.push(selection);
+      } else {
+        $scope.selected.slice(idx)
+      }
+
       $scope.choices[idx].selected = !$scope.choices[idx].selected
     }
 
